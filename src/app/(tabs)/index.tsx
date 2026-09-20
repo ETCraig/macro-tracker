039ededa6832ed85bfea121f98ac78ac9ffb36cd
ETCraig <dev.etcraig@gmail.com>
2026-09-19 19:54:52 -0500

    feat: installed async-storage and implamented into homne screen and add meal

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
+import HomeHeader from "@/components/HomeHeader";
 import MacroGrid from "@/components/MacroGrid";
 import RecentMeals from "@/components/RecentMeals";
+import { getMeals, Meal } from "@/storage/meals";
 import { globalStyles } from "@/styles/global";
-import { Text, View } from "react-native";
-import HomeHeader from "../../components/HomeHeader";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
+import { ScrollView, Text } from "react-native";
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log("Loaded meals:", data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
-    <View style={globalStyles.container}>
+    <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <MacroGrid />
-      <RecentMeals />
-    </View>
+      <MacroGrid meals={meals} />
+      <RecentMeals meals={meals} />
+    </ScrollView>
   );
 }
